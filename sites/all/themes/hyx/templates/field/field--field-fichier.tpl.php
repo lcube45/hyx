91db91e661868aa040a31056a8c684c1f4ef0dd8
Laurent Le Lardeux <lcube@MacBook-Pro-de-Laurent.local>
2016-07-22 18:37:46 +0200

    update drupal 7.5

--- sites/all/themes/hyx/templates/field/field--field-fichier.tpl.php
+++ sites/all/themes/hyx/templates/field/field--field-fichier.tpl.php
@@ -6,10 +6,57 @@
  * @see http://developers.whatwg.org/grouping-content.html#the-ul-element
  */
 ?>
-<div class="row">
+<div class="row links">
   <?php foreach ($items as $delta => $item): ?>
     <div class="col-sm-4 col-xs-12">
-      <?php print render($item); ?>
+
+      <?php
+        $image = array(
+          'style_name' => 'livre_extrait',
+          'path' => $item['#file']->uri,
+          'alt' => $item['#file']->alt,
+          'title' => $item['#file']->title
+        );
+      ?>
+
+      <a href="<?php print file_create_url($item['#file']->uri); ?>" title="<?php $item['#file']->title; ?>" data-gallery>
+        <?php print theme('image_style', $image); ?>
+      </a>
+
     </div>
   <?php endforeach; ?>
+</div>
+
+<div id="blueimp-gallery" class="blueimp-gallery" data-use-bootstrap-modal="false">
+    <!-- The container for the modal slides -->
+    <div class="slides"></div>
+    <!-- Controls for the borderless lightbox -->
+    <h3 class="title"></h3>
+    <a class="prev">‹</a>
+    <a class="next">›</a>
+    <a class="close">×</a>
+    <a class="play-pause"></a>
+    <ol class="indicator"></ol>
+    <!-- The modal dialog, which will be used to wrap the lightbox content -->
+    <div class="modal fade">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" aria-hidden="true">&times;</button>
+                    <h4 class="modal-title"></h4>
+                </div>
+                <div class="modal-body next"></div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default pull-left prev">
+                        <i class="glyphicon glyphicon-chevron-left"></i>
+                        Previous
+                    </button>
+                    <button type="button" class="btn btn-primary next">
+                        Next
+                        <i class="glyphicon glyphicon-chevron-right"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
